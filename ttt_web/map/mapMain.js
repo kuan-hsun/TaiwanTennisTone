8860f6ffff64d8d94a16d15ef8b76d4016bed5c8
Kuan-Hsun Cho <33713306+kuan-hsun@users.noreply.github.com>
2020-01-25 22:11:55 +0800

    First commit

---
+++ ttt_web/map/mapMain.js
@@ -0,0 +1,13 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([121, 23.5]),
+        zoom: 8
+    })
+});
+
